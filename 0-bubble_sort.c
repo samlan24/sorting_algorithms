e9a8a38e185ff24cad6a8959f986a4ecb3c2c1b0
samlan24 <muchemionline@gmail.com>
2024-04-27 07:59:03 -0700

    changes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,12 +23,15 @@ void swap(int *a, int *b)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int swapped;
 
 	if (array == NULL || size == 0)
 		return;
-
+	
 	for (i = 0; i < size - 1; i++)
 	{
+		
+		int swapped = false;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
